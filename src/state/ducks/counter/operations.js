8ffc17398728b0469e650d5c042dbea19b183ce0
Joonas Tiala <joonas.tiala@gmail.com>
2018-06-06 15:28:05 +0300

    Simplify operations.js

--- src/state/ducks/counter/operations.js
+++ src/state/ducks/counter/operations.js
@@ -1,5 +1,2 @@
-import { increment, decrement, reset } from './actions';
-import { resetAndGotoHome } from './thunks';
-
-export { increment, decrement, reset };
-export { resetAndGotoHome };
+export { increment, decrement, reset } from './actions';
+export { resetAndGotoHome } from './thunks';
