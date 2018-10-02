68a5952d1d94837e9c99f5796b448a3522c2c88c
Joonas Tiala <joonas.tiala@gmail.com>
2018-10-02 12:25:05 +0300

    Allow redux dev tools underscore dangles

--- src/state/store.js
+++ src/state/store.js
@@ -14,7 +14,6 @@ export const configureStore = (initialState = StoreRecord()) => {
   const middleware = [restMiddleware, thunk, routerMiddleware(history)];
 
   if (process.env.NODE_ENV === 'development') {
-    // eslint-disable-next-line no-underscore-dangle
     const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
 
     if (typeof devToolsExtension === 'function') {
