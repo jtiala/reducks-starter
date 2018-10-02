59ae5fb163464a91a0b73df9ecfcf77361cb5d97
Joonas Tiala <joonas.tiala@gmail.com>
2018-10-02 12:51:24 +0300

    Add button to fetch repos using a mock

--- src/views/components/RepositoryList/RepositoryList.jsx
+++ src/views/components/RepositoryList/RepositoryList.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { OrderedSet } from 'immutable';
+import { mocks } from '../../../state/apis/github';
 import Button from '../Button';
 import styles from './RepositoryList.scss';
 
 const RepositoryList = ({ fetchRepositories, hasRepositories, repositories }) => (
   <div className={styles.root}>
-    {hasRepositories ? (
+    <Button onClick={() => fetchRepositories()}>Fetch public repositories from GitHub</Button>
+    <Button onClick={() => fetchRepositories(mocks.getRepositoriesSuccess.body)}>
+      Fetch public repositories from GitHub (using a mock)
+    </Button>
+    {hasRepositories && (
       <ul>
         {repositories.map((repository) => (
-          <li key={`repo-${repository.id}`}>{repository.name}</li>
+          <li key={`repo-${repository.id}`}>
+            <a href={repository.html_url} target="_blank" rel="noopener noreferrer">
+              {repository.name}
+            </a>
+          </li>
         ))}
       </ul>
-    ) : (
-      <Button onClick={fetchRepositories}>Fetch public repositories from GitHub</Button>
     )}
   </div>
 );
