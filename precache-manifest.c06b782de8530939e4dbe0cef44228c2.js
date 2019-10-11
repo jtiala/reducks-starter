79a16cbe385d5bb8e3e31435a0eb47aae7f1e032
jtiala <joonas.tiala@gmail.com>
2019-10-11 09:37:07 +0000

    Deploying to gh-pages from master 0db3dd2cac73401ee7754f88e1db4ae669136529

---
+++ precache-manifest.c06b782de8530939e4dbe0cef44228c2.js
@@ -0,0 +1,57 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c970df218c827d4df5b830353c4890f0",
+    "url": "/./index.html"
+  },
+  {
+    "revision": "3906c37a3d4b0f337bf64ac3e37b8b4c",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "9ad977f1c85371140ddccfbe0e51976f",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "8b2706a3ce61e8f55887079509bad25e",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "e36975bf16da097896b26eac89a6711d",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "9dd869f69f7179ded08d80d6ce7562af",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "7ee30700f4939a97f0dde9c59a0d5a95",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "d223e7b151aba805d7de84c0f9854140",
+    "url": "/favicon.ico"
+  },
+  {
+    "url": "/main.97a05125b4a518e18f4f.css"
+  },
+  {
+    "url": "/main.97a05125b4a518e18f4f.js"
+  },
+  {
+    "revision": "2fbf6014a7ed83e35c0d79d10188fcc4",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "2746f0d3599151b33e9044d5085a2200",
+    "url": "/site.webmanifest"
+  },
+  {
+    "url": "/vendor.97a05125b4a518e18f4f.css"
+  },
+  {
+    "url": "/vendor.97a05125b4a518e18f4f.js"
+  },
+  {
+    "url": "/vendor.97a05125b4a518e18f4f.js.LICENSE"
+  }
+]);
